9d065c13514934aa887718cd167e9fe14d799d97
sarah801 <sarah2017578@miuegypt.edu.eg>
2024-04-21 22:23:17 +0200

    firebase

---
+++ lib/DB_Tier/firebase/firebase_firestore.dart
@@ -0,0 +1,5 @@
+//This file would contain methods related to reading,
+ //writing, updating, and deleting data in Firestore,
+ // including user and admin management.
+
+ //import 'package:cloud_firestore/cloud_firestore.dart';

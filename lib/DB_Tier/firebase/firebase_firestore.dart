1a6fe58e7720e31e6fb8a4013570b64113966007
MayarMohamed7 <mayar2012932@miuegypt.edu.eg>
2024-05-03 03:15:42 +0300

    saveUserData func fix

--- lib/DB_Tier/firebase/firebase_firestore.dart
+++ lib/DB_Tier/firebase/firebase_firestore.dart
@@ -1,14 +1,14 @@
 //This file would contain methods related to reading,
- //writing, updating, and deleting data in Firestore,
- // including user and admin management.
+//writing, updating, and deleting data in Firestore,
+// including user and admin management.
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:graduationinterface/applicationTier/models/skill.dart';
 
- class FirestoreMethods {
+class FirestoreMethods {
   // Save the skills to Firestore
 
-  Future<void> saveSkillToFirestore(List<String> skills) async  //for SkillsPage 
+  Future<void> saveSkillToFirestore(List<String> skills) async //for SkillsPage
   {
     try {
       // Get the current user's ID
@@ -24,7 +24,9 @@ import 'package:graduationinterface/applicationTier/models/skill.dart';
 
         if (userSnapshot.exists) {
           // If the user document already exists, update the skills field
-          List<String>? existingSkills = (userSnapshot.data() as Map<String, dynamic>)['skills']?.cast<String>();
+          List<String>? existingSkills =
+              (userSnapshot.data() as Map<String, dynamic>)['skills']
+                  ?.cast<String>();
           List<String> updatedSkills = [...existingSkills ?? [], ...skills];
 
           await userDocRef.update({'skills': updatedSkills});
@@ -38,48 +40,85 @@ import 'package:graduationinterface/applicationTier/models/skill.dart';
       // Handle error
     }
   }
-Future<List<Skill>> fetchSkillsFromFirestore() async // for MY skills page
-{
-  List<Skill> skills = [];
 
-  try {
-    String? userId = FirebaseAuth.instance.currentUser?.uid;
-    if (userId != null) {
-      DocumentSnapshot userSnapshot =
-          await FirebaseFirestore.instance.collection('users').doc(userId).get();
-      if (userSnapshot.exists) {
-    List<dynamic> skillsData = (userSnapshot.data() as Map<String, dynamic>)['skills'] ?? [];
+  Future<String?> saveUserData({
+    required String phoneNumber,
+    required String age,
+    required String educationalLevel,
+    required String jobStatus,
+    required String fieldOfInterests,
+  }) async {
+    try {
+      String? userId = FirebaseAuth.instance.currentUser?.uid;
+      if (userId != null) {
+        DocumentReference userDocRef =
+            FirebaseFirestore.instance.collection('userData').doc(userId);
 
-        skills = skillsData.map((skillName) => Skill(name: skillName)).toList();
+        Map<String, dynamic> userData = {
+          'phoneNumber': phoneNumber,
+          'age': age,
+          'educationalLevel': educationalLevel,
+          'jobStatus': jobStatus,
+          'fieldOfInterests': fieldOfInterests,
+        };
+
+        await userDocRef.set(userData);
+        return null; // Return null to indicate success
+      } else {
+        return 'User not logged in';
       }
+    } catch (e) {
+      return e.toString(); // Return error message
     }
-  } catch (error) {
-    print('Error fetching skills: $error');
   }
 
-  return skills;
-}
-Future<void> removeSkillFromFirestore(String skillName) async {
-  try {
-    String? userId = FirebaseAuth.instance.currentUser?.uid;
-    if (userId != null) {
-      DocumentReference userDocRef =
-          FirebaseFirestore.instance.collection('users').doc(userId);
+  Future<List<Skill>> fetchSkillsFromFirestore() async // for MY skills page
+  {
+    List<Skill> skills = [];
 
-      DocumentSnapshot userSnapshot = await userDocRef.get();
-      if (userSnapshot.exists) {
-        List<String> existingSkills =
-            (userSnapshot.data() as Map<String, dynamic>)['skills']?.cast<String>() ?? [];
+    try {
+      String? userId = FirebaseAuth.instance.currentUser?.uid;
+      if (userId != null) {
+        DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userId)
+            .get();
+        if (userSnapshot.exists) {
+          List<dynamic> skillsData =
+              (userSnapshot.data() as Map<String, dynamic>)['skills'] ?? [];
 
-        existingSkills.remove(skillName);
-
-        await userDocRef.update({'skills': existingSkills});
+          skills =
+              skillsData.map((skillName) => Skill(name: skillName)).toList();
+        }
       }
+    } catch (error) {
+      print('Error fetching skills: $error');
     }
-  } catch (error) {
-    print('Error removing skill: $error');
+
+    return skills;
+  }
+
+  Future<void> removeSkillFromFirestore(String skillName) async {
+    try {
+      String? userId = FirebaseAuth.instance.currentUser?.uid;
+      if (userId != null) {
+        DocumentReference userDocRef =
+            FirebaseFirestore.instance.collection('users').doc(userId);
+
+        DocumentSnapshot userSnapshot = await userDocRef.get();
+        if (userSnapshot.exists) {
+          List<String> existingSkills =
+              (userSnapshot.data() as Map<String, dynamic>)['skills']
+                      ?.cast<String>() ??
+                  [];
+
+          existingSkills.remove(skillName);
+
+          await userDocRef.update({'skills': existingSkills});
+        }
+      }
+    } catch (error) {
+      print('Error removing skill: $error');
+    }
   }
 }
- }
- 
- 

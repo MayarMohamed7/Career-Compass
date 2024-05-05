8e523e03b057bfe3541185332b4b31c3c7a65786
sarah801 <sarah2017578@miuegypt.edu.eg>
2024-05-06 01:15:49 +0300

    shared prefs

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:graduationinterface/presentationTier/Pages/intro.dart';
 import 'package:graduationinterface/DB_Tier/firebase/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart'; // Import Firebase Core package
+import 'package:graduationinterface/presentationTier/Pages/Optional.dart'; 
+import 'package:graduationinterface/DB_Tier/firebase/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: IntroPage(),
-      debugShowCheckedModeBanner: false,
+    return FutureBuilder<bool>(
+      future: AuthMethods().checkAuthenticationStatus(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        } else {
+          final bool isLoggedIn = snapshot.data ?? false;
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: isLoggedIn ? OptionalPage() : IntroPage(),
+          );
+        }
+      },
     );
   }
 }
-

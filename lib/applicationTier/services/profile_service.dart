3ba929f8d3630b09624f2701c30eaacd527737d6
sarah801 <sarah2017578@miuegypt.edu.eg>
2024-05-10 04:56:15 +0300

    profile edits

--- lib/applicationTier/services/profile_service.dart
+++ lib/applicationTier/services/profile_service.dart
@@ -1,34 +1,66 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-
 class ProfileService {
-  
-      bool PasswordsMatched(String password, String confirmPassword) {
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+      bool PasswordsMatched(String password, String confirmPassword)
+   {
     return password == confirmPassword;
   }
-
-// Function to update user profile email and password
-  Future<void> updateProfile(String fullName, String email, String password, String confirmPassword) async {
-    final user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      if (PasswordsMatched(password, confirmPassword)) {
-        await user.updateEmail(email);
-        await user.updatePassword(password);
-        await FirebaseFirestore.instance.collection('users').doc(user.uid).update({
-          'fullName': fullName,
-          'email': email,
-        });
-      }
-    }
+Future<Map<String, dynamic>> getUserData(String userId) async {
+    final userDataDoc = await _firestore.collection('userData').doc(userId).get();
+    return userDataDoc.data() ?? {};
   }
-   // function to update profile Item 
 
-  Future<void> updateProfileItem(String uid, String field, String value) async {
-    await FirebaseFirestore.instance.collection('userData').doc(uid).update({
-      field: value,
-    }); 
+Future<Map<String, dynamic>> getCred(String userId) async {
+    final userDataDoc = await _firestore.collection('users').doc(userId).get();
+    return userDataDoc.data() ?? {};
+  }
 
+  Future<void> updateUserData(String userId, Map<String, dynamic> newData) async {
+    await _firestore.collection('userData').doc(userId).update(newData);
+  }
+Future<void> updateCred(String userId, Map<String, dynamic> newData) async {
+    await _firestore.collection('users').doc(userId).update(newData);
+  }
   
 }
+  
+  
+
+  
+// Function to fetch user profile data
+
+  /*Future<User?> getCurrentUserProfile() async {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  User? currentUser = auth.currentUser;
+  if (currentUser != null) {
+    // Fetch user data from 'users' table
+    DocumentSnapshot userSnapshot = await FirebaseFirestore.instance.collection('users').doc(currentUser.uid).get();
+    Map<String, dynamic> userData = userSnapshot.data() as Map<String, dynamic>;
+
+    // Fetch user profile data from 'userData' table
+    DocumentSnapshot userProfileSnapshot = await FirebaseFirestore.instance.collection('userData').doc(currentUser.uid).get();
+    Map<String, dynamic> userProfileData = userProfileSnapshot.data() as Map<String, dynamic>;
+
+    // Merge data from both tables into a single User object
+    return User(
+      id: userData['id'],
+      email: userData['email'] ?? '',
+      fullName: userData['fullName'] ?? '',
+      phoneNumber: userProfileData['phoneNumber'],
+      age: userProfileData['age'],
+      educationalLevel: userProfileData['educationalLevel'],
+      jobStatus: userProfileData['jobStatus'],
+      fieldOfInterests: userProfileData['fieldOfInterests'],
+    );
+  }
+  return null;
 }
+
+Future<void> fetchUserProfile(Function(User?) onProfileFetched) async {
+    User? userProfile = await getCurrentUserProfile();
+    onProfileFetched(userProfile);
+  }*/
--- lib/presentationTier/Pages/profile_content.dart
+++ lib/presentationTier/Pages/profile_content.dart
@@ -1,13 +1,43 @@
-// presentationTier/pages/profile_page.dart
 import 'package:flutter/material.dart';
-import 'package:graduationinterface/presentationTier/Widgets/drawer.dart';
-import 'package:graduationinterface/presentationTier/Widgets/footer.dart';
-import 'package:graduationinterface/presentationTier/Widgets/profile_item.dart';
-import 'package:graduationinterface/presentationTier/Forms/profile_page.dart';
 import 'package:graduationinterface/applicationTier/services/CustomImagePicker.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:typed_data';
+import 'package:graduationinterface/DB_Tier/firebase/firebase_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
-class ProfileContent extends StatelessWidget {
+
+class ProfileContent extends StatefulWidget {
+  @override
+  _ProfileContentState createState() => _ProfileContentState();
+}
+
+class _ProfileContentState extends State<ProfileContent> {
+  CustomImagePicker _customImagePicker = CustomImagePicker();
+  Uint8List? _imageBytes;
+  StorageMethods _storageMethods = StorageMethods();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+    User? user = FirebaseAuth.instance.currentUser;
+    String? imageUrl;
+    @override
+    void initState() {
+    super.initState();  
+    _loadImageUrlFromStorage();
+  }
+
+  Future<void> _loadImageUrlFromStorage() async {
+    try {
+      String path = 'profile_images/${_auth.currentUser!.email}.jpg';
+      String? url = await _storageMethods.getImageUrlFromStorage(path);
+      setState(() {
+        imageUrl = url;
+      });
+    } catch (e) {
+      print('Error retrieving image URL from storage: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -29,33 +59,72 @@ class ProfileContent extends StatelessWidget {
               ),
               child: CircleAvatar(
                 radius: 50,
-                backgroundImage:  AssetImage('images/assets/8.png'),
+                backgroundImage: _imageBytes != null
+                    ? MemoryImage(_imageBytes!)
+                    : imageUrl != null
+                        ? NetworkImage(imageUrl!)
+                        : AssetImage('images/assets/8.png') as ImageProvider<Object>?,
               ),
             ),
             Positioned(
               bottom: 0,
               right: 0,
-              child: PopupMenuButton<String>(
-                onSelected: (value) {
-                  if (value == 'camera') {
-                    CustomImagePicker().pickImage(ImageSource.camera);
-                  } else {
-                    CustomImagePicker().pickImage(ImageSource.gallery);
-                  }
-                },
-                itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-                  const PopupMenuItem<String>(
-                    value: 'camera',
+              child: PopupMenuButton(
+                itemBuilder: (context) => [
+                  PopupMenuItem(
                     child: ListTile(
-                      leading: Icon(Icons.camera_alt),
-                      title: Text('Camera'),
+                      leading: const Icon(Icons.camera_alt),
+                      title: const Text('Camera'),
+                      onTap: () async {
+                        Uint8List? image =
+                            await _customImagePicker.pickImage(ImageSource.camera);
+                        if (image != null) {
+                          try {
+                            String path =
+                                'profile_images/${_auth.currentUser!.email}.jpg';
+                            imageUrl = await _storageMethods
+                                .uploadImageToStorage(path, image);
+                            SharedPreferences prefs =
+                                await SharedPreferences.getInstance();
+                            await prefs.setString(
+                                'profile_image_url', imageUrl ?? '');
+                            setState(() {
+                              _imageBytes = image;
+                            });
+                          } catch (e) {
+                            print('Error uploading image and saving URL: $e');
+                            // Handle error gracefully (e.g., show a snackbar)
+                          }
+                        }
+                      },
                     ),
                   ),
-                  const PopupMenuItem<String>(
-                    value: 'gallery',
+                  PopupMenuItem(
                     child: ListTile(
-                      leading: Icon(Icons.photo_library),
-                      title: Text('Gallery'),
+                      leading: const Icon(Icons.photo_library),
+                      title: const Text('Gallery'),
+                      onTap: () async {
+                        Uint8List? image =
+                            await _customImagePicker.pickImage(ImageSource.gallery);
+                        if (image != null) {
+                          try {
+                            String path =
+                                'profile_images/${_auth.currentUser!.email}.jpg';
+                            imageUrl = await _storageMethods
+                                .uploadImageToStorage(path, image);
+                            SharedPreferences prefs =
+                                await SharedPreferences.getInstance();
+                            await prefs.setString(
+                                'profile_image_url', imageUrl ?? '');
+                            setState(() {
+                              _imageBytes = image;
+                            });
+                          } catch (e) {
+                            print('Error uploading image and saving URL: $e');
+                            // Handle error gracefully (e.g., show a snackbar)
+                          }
+                        }
+                      },
                     ),
                   ),
                 ],
@@ -68,4 +137,3 @@ class ProfileContent extends StatelessWidget {
     );
   }
 }
-
